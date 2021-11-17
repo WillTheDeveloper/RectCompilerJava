c168595556019bc72eec7d19d15d64d4910e21d1
Will <twitchifygamers@gmail.com>
2021-11-17 20:36:17 +0000

    More test stuff

--- Main.java
+++ Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         String name = "Will";
         System.out.println("Hello " + name);
+        System.out.println("Length of " + name + " is " + name.length() + " characters.");
+        if (name.length() > 5) {
+            System.out.println("You have a pretty long name.");
+        } else {
+            System.out.println("Your name is quite short.");
+        }
     }
 }
